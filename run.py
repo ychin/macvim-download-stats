b5cf9e0d57fdd467080fc34b1cb34bad46702a38
Yee Cheng Chin <ychin.git@gmail.com>
2023-06-13 18:45:33 -0700

    Add a releases.csv table for overall table metadata
    This table helps store misc useful metadata about a release in a table,
    so it will be easier to use when say importing to a spreadsheet program.
    
    Python requirements is now mandated at 3.11+ because it has a better
    datetime.fromisoformat() util, and I don't want to add dateutil which is
    an external pip dependency.

--- run.py
+++ run.py
@@ -5,21 +5,32 @@
 # download/install count history.
 #
 # Store the output in CSV files to make them easy to import to other tools.
+#
+# Note: This file needs Python 3.11+ to get the more flexible
+# datetime.fromisoformat() implementation. If we have to run on 3.10 or below,
+# we could add the dateutil package as a dependency.
 
+from datetime import datetime, timezone
 from urllib.request import Request, urlopen
-import datetime
 import csv
+import json
 import os
 import os.path
-import json
+import sys
 
-utc_date_header = 'Date (UTC)'
+if sys.version_info.major == 3 and sys.version_info.minor <= 10:
+    # We need Python 3.11 to run properly. See notes section above.
+    print(f'Python version is too old (need to be 3.11+): {sys.version}', file=sys. stderr)
+    exit(-100)
+
+utc_date_header = "Date (UTC)"
 
 # Extract the current UTC time, and store in a sort-of ISO format. Don't use
 # the official YYYY-MM-DDTHH:MM:SS format or add timezone info, because those
 # will confuse Excel or Google Spreadsheets during import as those tools don't
 # have good date/time support.
-now_time = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+now_time = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
+
 
 def query_github_releases():
     # Using GitHub Token is optional as releases is public. Using a token helps prevent rate limiting.
@@ -35,31 +46,57 @@ def query_github_releases():
 
     first_item = True
 
+    # Release info to gather in a separate table
+    new_release_rows = []
+    release_columns = [
+        "id",
+        "tag_name",
+        "name",
+        "draft",
+        "prerelease",
+        "created_at",
+        "published_at",
+    ]
+
     # Loop through all releases
     for release in releases:
+        # Gather release info, and fix up date time to be spreadsheet parsable
+        release_row = {c: release[c] for c in release_columns}
+        for field in ["created_at", "published_at"]:
+            release_row[field] = (
+                datetime.fromisoformat(release_row[field])
+                .astimezone(timezone.utc)
+                .strftime("%Y-%m-%d %H:%M:%S")
+            )
+        new_release_rows.append(release_row)
+
         # Add a row of download counts for each asset to the CSV. Make the CSV file
         # if it doesn't exist. If one is already there, parses it to make sure the
         # header matches the assets we have. The logic here is slightly complicated
         # just to make sure if we added/removed assets from this release, the
         # column order will still make sense.
 
-        release_csv_path = os.path.join('github_release/downloads', f'{release["tag_name"]}.csv')
-        release_json_path = os.path.join('github_release/info', f'{release["tag_name"]}.json')
+        release_csv_path = os.path.join(
+            "github_release/downloads", f'{release["tag_name"]}.csv'
+        )
+        release_json_path = os.path.join(
+            "github_release/info", f'{release["tag_name"]}.json'
+        )
         new_release_csv = not os.path.exists(release_csv_path)
 
-        asset_names = [ asset['name'] for asset in release['assets'] ]
+        asset_names = [asset["name"] for asset in release["assets"]]
         if len(asset_names) == 0:
             continue
 
-        existing_rows = [] # Only used when need to convert CSV file
+        existing_rows = []  # Only used when need to convert CSV file
 
         if new_release_csv:
             os.makedirs(os.path.dirname(release_csv_path), exist_ok=True)
-            with open(release_csv_path, 'w', newline='') as release_csv_file:
+            with open(release_csv_path, "w", newline="") as release_csv_file:
                 writer = csv.writer(release_csv_file)
                 writer.writerow([utc_date_header] + asset_names)
         else:
-            with open(release_csv_path, 'r', newline='') as release_csv_file:
+            with open(release_csv_path, "r", newline="") as release_csv_file:
                 reader = csv.DictReader(release_csv_file)
                 header = reader.fieldnames
                 if reader.fieldnames == None:
@@ -69,65 +106,116 @@ def query_github_releases():
                 old_asset_names = reader.fieldnames[1:]
                 old_asset_names_set = set(old_asset_names)
 
-                new_asset_names = [n for n in asset_names if n not in old_asset_names_set]
-                asset_names = old_asset_names + [n for n in asset_names if n not in old_asset_names_set]
+                new_asset_names = [
+                    n for n in asset_names if n not in old_asset_names_set
+                ]
+                asset_names = old_asset_names + [
+                    n for n in asset_names if n not in old_asset_names_set
+                ]
 
                 if len(new_asset_names) > 0:
                     # We have added new assets to this release. We need to convert
                     # the CSV file because we need to change the header, which also
                     # means we need to read the rest of the file so we can write
                     # them back later.
-                    print(f'Release {release["tag_name"]} added assets f{new_asset_names}. Converting CSV file.')
-                    existing_rows = [ row for row in reader ]
+                    print(
+                        f'Release {release["tag_name"]} added assets f{new_asset_names}. Converting CSV file.'
+                    )
+                    existing_rows = [row for row in reader]
 
         # We only append to the end of the file unless we have added assets forcing
         # us to re-write the header.
-        file_mode = 'w' if len(existing_rows) > 0 else 'a'
+        file_mode = "w" if len(existing_rows) > 0 else "a"
 
-        with open(release_csv_path, file_mode, newline='') as release_csv_file:
-            writer = csv.DictWriter(release_csv_file, fieldnames=[utc_date_header]+asset_names)
+        with open(release_csv_path, file_mode, newline="") as release_csv_file:
+            writer = csv.DictWriter(
+                release_csv_file, fieldnames=[utc_date_header] + asset_names
+            )
 
             if len(existing_rows) > 0:
                 writer.writeheader()
                 writer.writerows(existing_rows)
 
-            row = { name: download_count for (name, download_count) in [ (asset['name'], asset['download_count']) for asset in release['assets'] ] }
+            row = {
+                name: download_count
+                for (name, download_count) in [
+                    (asset["name"], asset["download_count"])
+                    for asset in release["assets"]
+                ]
+            }
 
             if first_item:
-                print('  ' + str(row))
+                print("  " + str(row))
 
             row[utc_date_header] = now_time
             writer.writerow(row)
 
         # Also write out the release's JSON just for reference instead of needing query GitHub in the future
         os.makedirs(os.path.dirname(release_json_path), exist_ok=True)
-        with open(release_json_path, 'w') as release_json_file:
+        with open(release_json_path, "w") as release_json_file:
             json.dump(release, release_json_file, indent=2)
 
         first_item = False
 
+    # Output the releases info into its own table so we can look up metadata like publish date etc
+    releases_info_csv_path = "github_release/releases.csv"
+    if os.path.exists(releases_info_csv_path):
+        with open(releases_info_csv_path, "r", newline="") as releases_info_file:
+            reader = csv.DictReader(releases_info_file)
+            old_release_rows = [row for row in reader]
+
+        old_release_ids = {release["id"] for release in old_release_rows}
+
+        release_rows = old_release_rows
+        release_rows += [r for r in new_release_rows if str(r['id']) not in old_release_ids]
+    else:
+        release_rows = new_release_rows
+
+    release_rows = sorted(release_rows, key = lambda o: o['id'])
+
+    with open(releases_info_csv_path, "w", newline="") as releases_info_file:
+        writer = csv.DictWriter(releases_info_file, fieldnames=release_columns)
+        writer.writeheader()
+        writer.writerows(release_rows)
+
+
 def query_homebrew_installs():
-    response = urlopen('https://formulae.brew.sh/api/formula/macvim.json')
+    response = urlopen("https://formulae.brew.sh/api/formula/macvim.json")
     formula_info = json.loads(response.read())
 
-    generated_date = formula_info['generated_date']
-    version = formula_info['versions']['stable']
-    num_installs = formula_info['analytics']['install']['30d']['macvim']
-    num_installs_on_request = formula_info['analytics']['install_on_request']['30d']['macvim']
+    generated_date = formula_info["generated_date"]
+    version = formula_info["versions"]["stable"]
+    num_installs = formula_info["analytics"]["install"]["30d"]["macvim"]
+    num_installs_on_request = formula_info["analytics"]["install_on_request"]["30d"][
+        "macvim"
+    ]
 
-    csv_path = os.path.join('homebrew', 'installs.csv')
+    csv_path = os.path.join("homebrew", "installs.csv")
 
     if not os.path.exists(csv_path):
         os.makedirs(os.path.dirname(csv_path), exist_ok=True)
-        with open(csv_path, 'w', newline='') as csv_file:
+        with open(csv_path, "w", newline="") as csv_file:
             writer = csv.writer(csv_file)
-            writer.writerow([utc_date_header, 'generated_date', 'versions.stable', 'install.30d', 'install_on_request.30d'])
+            writer.writerow(
+                [
+                    utc_date_header,
+                    "generated_date",
+                    "versions.stable",
+                    "install.30d",
+                    "install_on_request.30d",
+                ]
+            )
 
-    with open(csv_path, 'a', newline='') as csv_file:
+    with open(csv_path, "a", newline="") as csv_file:
         writer = csv.writer(csv_file)
-        writer.writerow([now_time, generated_date, version, num_installs, num_installs_on_request])
-    
-    print(f'  num_installs: {num_installs}, num_installs_on_request: {num_installs_on_request}, version: {version}')
+        writer.writerow(
+            [now_time, generated_date, version, num_installs, num_installs_on_request]
+        )
+
+    print(
+        f"  num_installs: {num_installs}, num_installs_on_request: {num_installs_on_request}, version: {version}"
+    )
+
 
 if __name__ == "__main__":
     print("Querying GitHub release stats.")
